a7eaec9ebf32882a3fb91558b3cd6f8d823911ec
Jackson Meyer <jmeyerer18@gmail.com>
2025-02-01 15:36:22 -0600

    Move CheckoutItem class outside of useCheckout declaration

--- composables/useCheckout.ts
+++ composables/useCheckout.ts
@@ -1,20 +1,24 @@
-export default function useCheckout() {
-    class CheckoutItem {
-        title: string;
-        image: string;
-        description: string;
-        price: number;
+class CheckoutItem {
+    title: string;
+    image: string;
+    description: string;
+    price: number;
 
-        constructor(title: string, image: string, description: string, price: number,) {
-            this.title = title;
-            this.image = image
-            this.description = description
-            this.price = price
-        }
+    constructor(title: string, image: string, description: string, price: number,) {
+        this.title = title;
+        this.image = image
+        this.description = description
+        this.price = price
     }
+}
+
+export default function useCheckout() {
     
     
     
     
-    return {}
+    
+    return {
+
+    }
 }
