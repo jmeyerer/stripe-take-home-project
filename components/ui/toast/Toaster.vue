3039409a33199699e05c36f463ef9f18b9a300c4
Jackson Meyer <jmeyerer18@gmail.com>
2025-02-04 21:05:57 -0600

    Switch to img tag in toasts
    - Missed the NuxtImg in the toast template

--- components/ui/toast/Toaster.vue
+++ components/ui/toast/Toaster.vue
@@ -11,13 +11,13 @@ const { toasts } = useToast()
     <Toast v-for="toast in toasts" :key="toast.id" v-bind="toast">
       <div class="grid gap-1">
         <div class="w-fit flex space-x-5 items-center">
-          <NuxtImg
+          <img
             v-if="toast.image"
             :src="toast.image"
             width="50"
             height="50"
             class="rounded-md w-fit h-fit"
-          ></NuxtImg>
+          ></img>
           <div class="flex flex-col space-y-0.5">
             <ToastTitle v-if="toast.title" class="text-base">
               {{ toast.title }}
